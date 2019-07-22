9b04a3bef76c358bfe9cc8ee2e43376999e672d5
IedSoftworks <iedsoftworks@gmail.com>
2019-07-22 11:29:01 +0200

    Deleted Polyline

--- classes/Weapons.cs
+++ classes/Weapons.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace MovingEngine.classes
 {
@@ -10,21 +11,67 @@ namespace MovingEngine.classes
     {
         public static void Aim()
         {
-            globals.canvas.Children.Remove(radius);
-            radius = new Polyline();
-            radius.Points.Add(new Point(globals.middlepoint.X, globals.middlepoint.Y));
-            radius.Points.Add(new Point(globals.mouse_position.X, globals.mouse_position.Y));
-            radius.Stroke = Brushes.Blue;
-            radius.StrokeThickness = 2;
-            globals.canvas.Children.Add(radius);
 
             double line = Math.Sqrt(Math.Pow(globals.middlepoint.X - globals.mouse_position.X, 2) + Math.Pow(globals.middlepoint.Y - globals.mouse_position.Y, 2));
             double gegen = Math.Sqrt(Math.Pow(globals.middlepoint.X - globals.mouse_position.X, 2) + Math.Pow(line - globals.mouse_position.Y, 2));
-            double rad = Math.Sin(gegen / line) * 90 / Math.PI;
+            double rad = Math.Asin(gegen / line) * (90 / Math.PI);
 
             Console.WriteLine(gegen);
             RotateTransform rotateTransform = new RotateTransform(rad, 45, 45);
             globals.player.visual.RenderTransform = rotateTransform;
+        }
+        public static double XYToDegrees(double x, double y)
+        {
+            double ox = x;
+            double oy = y;
+            int plus = 0;
+            y *= -1;
+            if (x < 0)
+            {
+                plus = -90;
+            }
+            if (y < 0)
+            {
+                plus = 90;
+            }
+            if (x < 0 && y < 0)
+            {
+                plus = 180;
+            }
+            x = Math.Abs(x);
+            y = Math.Abs(y);
+            if (ox == 0 && oy == 0)
+            {
+                return 0;
+            }
+            if (ox == 0)
+            {
+                if (oy < 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 180;
+                }
+            }
+            if (oy == 0)
+            {
+                if (ox > 0)
+                {
+                    return 90;
+                }
+                else
+                {
+                    return 270;
+                }
+            }
+            double deg = Math.Asin(y / Math.Sqrt(x * x + y * y)) * (180 / Math.PI);
+            if (ox > 0 != oy > 0)
+            {
+                deg = 90 - deg;
+            }
+            return deg + plus;
         }
     }
 }
